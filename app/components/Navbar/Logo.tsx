e960c56d517caceac9c6d41474c090096cc74c7e
Elkana Hans <elknhns@gmail.com>
2023-06-20 13:10:27 +0700

    refactor: refactor logo

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
-export default function Logo() {
-	const router = useRouter();
+const Logo = () => (
+	<Image
+		src='/images/logo.png'
+		alt='Airbnb logo'
+		className='hidden md:block cursor-pointer'
+		height={100}
+		width={100}
+	/>
+);
 
-	return (
-		<Image
-			src='/images/logo.png'
-			alt='Airbnb logo'
-			className='hidden md:block cursor-pointer'
-			height={100}
-			width={100}
-		/>
-	);
-}
+export default Logo;
